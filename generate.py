469546b779bd3323aa0d87c21b5e400a9f826f06
Bernardo Lopes <steamloc@berna.zone>
2026-03-06 07:51:44 -0300

    Edit generation script

--- generate.py
+++ generate.py
@@ -5,9 +5,9 @@ def main():
     subprocess.run(["dotnet", "clean"])
     [subprocess.run(["dotnet", "publish", "-c", "Release", "-r", platform, "-p:PublishSingleFile=true"]) for platform in ["linux-x64", "win-x64", "osx-arm64"]]
     with ZipFile("deadlock-audio.zip", 'w') as zip:
-        zip.write("deadlock-caption-localizer-backend/bin/Release/net10.0/linux-x64/publish/deadlock-caption-localizer-backend")
-        zip.write("deadlock-caption-localizer-backend/bin/Release/net10.0/osx-arm64/publish/deadlock-caption-localizer-backend")
-        zip.write("deadlock-caption-localizer-backend/bin/Release/net10.0/win-x64/publish/deadlock-caption-localizer-backend.exe")
+        zip.write("deadlock-caption-localizer-backend/bin/Release/net10.0/linux-x64/publish/deadlock-caption-localizer-backend", "backend-linux")
+        zip.write("deadlock-caption-localizer-backend/bin/Release/net10.0/osx-arm64/publish/deadlock-caption-localizer-backend", "backend-macos")
+        zip.write("deadlock-caption-localizer-backend/bin/Release/net10.0/win-x64/publish/deadlock-caption-localizer-backend.exe", "backend-windows.exe")
         zip.write("ff-extension.xpi")
 if __name__ == "__main__":
     main()
